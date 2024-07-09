45ed6c302f849b6621786e5ad5c0dce143de76c6
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-07-09 02:38:02 +0000

    Numeric Processing (File Processing) Example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-// Chapter 5 Programming Examples, each example on a separate branch
+// Numeric Processing
+// This program reads random numbers from a file and displays the count of numbers on the file, the sum of the numbers
+// and the average of the numbers.
 #include <iostream>
+#include <fstream>
 
-int main() {
-  std::cout << "Chapter 5 Programming Examples\n";
+using namespace std;
+
+int main()
+{
+    const string filename = "Random.txt";
+
+    ifstream infile;
+    // Initialize counter and accumulator to 0
+    int count = 0, sum = 0, number;
+    double average;
+
+    infile.open(filename);
+    if (infile.fail())
+    {
+        cout << "Could not find " << filename << endl;
+        return(1);
+    }
+
+    while (infile >> number)
+    {
+        sum += number;
+        count++;
+    }
+
+    average = static_cast<double>(sum) / count;
+
+    cout << "Count of numbers on file: " << count << endl;
+    cout << "Sum of the numbers: " << sum << endl;
+    cout << "Average of the numbers: " << average << endl;
 }
