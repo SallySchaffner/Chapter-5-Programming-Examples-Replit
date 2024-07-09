04bd4ac350d8419c214a246b82a55110ef8d493a
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-07-09 02:31:18 +0000

    The Greatest and Least of These Example

--- .ccls-cache/@home@runner@Chapter-5-Programming-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-5-Programming-Examples/main.cpp
@@ -1,6 +1,30 @@
-// Chapter 5 Programming Examples, each example on a separate branch
+// The Greatest and Least of These
+// This program finds the largest and smallest number in a series of numbers entered by the user. 
 #include <iostream>
 
-int main() {
-  std::cout << "Chapter 5 Programming Examples\n";
+using namespace std;
+
+int main()
+{
+    const int SENTINEL = -99;
+    int minNumber, maxNumber, number;
+
+    cout << "Enter a series of numbers." << endl;
+    cout << "Type -99 to the series." << endl;
+    cin >> number;
+    minNumber = number;
+    maxNumber = number;
+    while (number != SENTINEL)
+    {
+        if (number < minNumber)
+            minNumber = number;
+        if (number > maxNumber)
+            maxNumber = number;
+        cin >> number;
+    }
+
+    cout << "The smallest number in the series was: " << minNumber << endl;
+    cout << "The largest number in the series was: " << maxNumber << endl;
+
+    return (0);
 }
