7bee94758ed92e82009ab79cd6cdb592417fc84e
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-07-09 02:34:20 +0000

    Sales Bar Chart Example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,41 @@
-// Chapter 5 Programming Examples, each example on a separate branch
-#include <iostream>
+//Sales Bar Chart
 
-int main() {
-  std::cout << "Chapter 5 Programming Examples\n";
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    const int AMOUNT_PER = 100.;
+
+    int storeNumber = 1, storeSales, numStars;
+    string fileName;
+    ifstream infile;
+
+    cout << "Please enter the name of the data file: ";
+    cin >> fileName;
+
+    infile.open(fileName);
+    if (infile.fail())
+    {
+        cout << "Could not open file " << fileName << endl;
+        return(1);
+    }
+
+    cout << "SALES BAR CHART" << endl;
+    cout << "(Each * = $100)" << endl;
+    //Use nested loops
+    while (infile >> storeSales)
+    {
+        cout << "Store " << storeNumber++ << ": ";
+        numStars = storeSales / AMOUNT_PER;
+        for (int j = 0; j < numStars; j++)
+            cout << "*";
+        cout << endl;
+    }
+
+    infile.close();
+
+    return(0);
+
 }
