9eb37d139c8e9a368921d1fe1bd563fba8f23077
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2024-07-09 02:29:09 +0000

    Celsius to Fahrenheit Example

--- .ccls-cache/@home@runner@Chapter-5-Programming-Examples/main.cpp
+++ .ccls-cache/@home@runner@Chapter-5-Programming-Examples/main.cpp
@@ -1,6 +1,27 @@
-// Chapter 5 Programming Examples, each example on a separate branch
-#include <iostream>
+// Celsius to Fahrenheit
+// This program produces a table of celsius and fahrenheit equivalent temperatures from 0 - 20 degrees Celsius.
 
-int main() {
-  std::cout << "Chapter 5 Programming Examples\n";
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  const int MIN_CELSIUS = 0;
+  const int MAX_CELSIUS = 20;
+  double fahrenheit;
+
+  cout << "Temperature in Celsius and Fahrenheit" << endl;
+  cout << "Celsius     Fahrenheit" << endl;
+  cout << "----------------------" << endl;
+  cout << fixed << setprecision(1) << showpoint;
+  for (int c = MIN_CELSIUS; c <= MAX_CELSIUS; c++)
+  {
+    fahrenheit = 9. / 5. * c + 32;
+    cout << setw(4) << c << setw(14) << fahrenheit << endl;
+
+  }
+
+  return(0);
 }
